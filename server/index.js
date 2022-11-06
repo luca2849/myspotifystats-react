a8a03d0a4faef5e09f8551bda66826a77e065c25
Luca Passariello <luca@lucapassariello.co.uk>
2022-11-06 09:11:06 +0000

    MSS-18 - Added BE route to access FE

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const config = require("config");
+const path = require("path");
 const app = express();
 
 // Init Middleware
@@ -8,6 +9,14 @@ app.use(express.json({ extend: false }));
 // Define Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/user", require("./routes/user"));
+
+// Route for built client routes
+app.use(express.static("../client/build"));
+app.get("*", (req, res) => {
+	res.sendFile(
+		require("path").resolve(__dirname, "../client", "build", "index.html")
+	);
+});
 
 const PORT = config.PORT || 5000;
 app.listen(PORT, () => {
