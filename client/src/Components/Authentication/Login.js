10984e10de70b407d5aa671c7385410dedec0769
Luca Passariello <luca@lucapassariello.co.uk>
2022-11-06 15:00:46 +0000

    MSS-19 - Fixed FE redirect URI

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 // page for functionality of authenticating with spotify via the API
 
 const Login = () => {
+	const loc = useLocation();
 	// setup spotify authentication url
 	const scope = "user-read-private user-read-email";
 	const params = new URLSearchParams({
 		response_type: "code",
 		client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
 		scope,
-		redirect_uri: "http://localhost:3000/login/callback",
+		redirect_uri: `${window.location.origin}/login/callback`,
 	}).toString();
 
 	const redirect_url = `https://accounts.spotify.com/authorize?${params}`;
